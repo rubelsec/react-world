2874e73b5a146725637cc36927b481d730f9b6cc
rubelsec <rubelmrm1492@gmail.com>
2026-04-07 13:12:59 +0600

    visited button  added this feature

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,19 +1,40 @@
 import { useEffect, useState } from "react";
 import Country from "./Country/Country";
-import './Countries.css';
+// import css
+import './Countries.css'
 const Countries = () => {
     const[countries,setCountries]=useState([]);
+    const [visitedCountries,setVisitedCountries]=useState([]);
+
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags,area,populations')
         .then (res=>res.json())
         .then(data=>setCountries(data));
     },[])
+    //mark visisted related  work
+
+const handleVisitedCountry=country=>{
+    console.log('add to this to your visited country');
+    console.log(country);
+}
+
     return (
-        <div className="country-container">
+        <div >
             <h3>Countries:{countries.length}</h3>
+            <div>
+                <h5>Visited countries</h5>
+                <ul>
+
+                </ul>
+            </div>
+            <div className="country-container">
             {
-              countries.map(country =><Country key={country.cca3} country={country}></Country> )  
+              countries.map(country =><Country 
+                key={country.cca3} 
+            handleVisitedCountry={handleVisitedCountry}
+                country={country}></Country> )  
             }
+            </div>
          
             
         </div>
--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,13 +1,24 @@
 
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
-   const{name,flags,population,area}=country;
+const Country = ({country,handleVisitedCountry}) => {
+     const{name,flags,population,area}=country;
+const [visited,setVisited]=useState(false);
+const handleVisited =() => {
+    setVisited(!visited);
+}
+console.log(handleVisitedCountry);
+
+  
     return (
-        <div className='country'>
-            <h3>Name:{name?.common}</h3>
+        <div className={`country ${visited? 'visited':'non-visite' }`}>
+            <h3 style={{ color: visited ? 'Purple' : 'White' }}>Name:{name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population:{population} </p>
             <p>area:{area}</p>
+            <button>Mark visited</button>
+            <button onClick={handleVisited}> {visited? 'visited':'Going'} </button><br />
+            {visited?   'I have visited this country.':'I want to visit'}
         </div>
     );
 };
